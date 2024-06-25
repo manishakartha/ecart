8bc3e11a578efbca21b0a18499a34030fc94faae
manishakartha <maneesha.kartha@gmail.com>
2024-06-26 01:08:43 +0200

    topnav

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TopNav from './Component/TopNav';
+import CatNav from './Component/CatNav';
+
+function App() {
+  return (
+    <div className="App">
+    <TopNav/>
+    <CatNav/>
+    </div>
+  );
+}
+
+export default App;
